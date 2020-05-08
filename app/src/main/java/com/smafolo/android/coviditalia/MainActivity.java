d656b9e76c7e3d4964949412c6e7b94f826ed27b
Filippo <filippoegizii@gmail.com>
2020-05-08 05:21:53 +0200

    Display a list view of fake regioni

--- app/src/main/java/com/smafolo/android/coviditalia/MainActivity.java
+++ app/src/main/java/com/smafolo/android/coviditalia/MainActivity.java
@@ -3,12 +3,36 @@ package com.smafolo.android.coviditalia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create a fake list of regioni.
+        ArrayList<String> regioni = new ArrayList<>();
+        regioni.add("Toscana");
+        regioni.add("Lombardia");
+        regioni.add("Lazio");
+        regioni.add("Sicilia");
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView regioniListView = findViewById(R.id.list_view);
+
+        // Create a new {@link ArrayAdapter} of regioni
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, regioni);
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        regioniListView.setAdapter(adapter);
+
     }
 }
